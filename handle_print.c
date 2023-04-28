76e8307f5d5310cf08abcb7898c07afede673673
rafmury <126829982+rafmury@users.noreply.github.com>
2023-04-28 23:43:07 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -9,6 +9,7 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
+ * by @ridhwan497 and @rafmury
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
