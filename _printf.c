4511cdd0a63930f794e83e8bfd7efab36cdc3fd4
rafmury <126829982+rafmury@users.noreply.github.com>
2023-04-28 14:51:40 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,107 @@
 #include "main.h"
-
 /**
  * _printf - produces output according to a format
- * @format: character string
+ * @format: format string
  * Return: number of characters printed
- * created by @ridhwan497 and @rafmury
  */
-
 int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
-	char *str, *start;
-	flags_t flags = FLAGS_INIT;
-
+	char *p = (char *)format;
+	char buffer[BUFF_SIZE] = {0};
+	int buffer_len = 0;
 	va_start(args, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (str = (char *)format; *str; str++)
+	while (*p)
 	{
-		initialize_flags(&flags, args);
-		if (*str != '%')
+		if (*p != '%')
 		{
-			count += _putchar(*str);
+			buffer[buffer_len++] = *p;
+			if (buffer_len == BUFF_SIZE)
+			{
+				count += write(STDOUT_FILENO, buffer, buffer_len);
+				buffer_len = 0;
+			}
+			p++;
+			count++;
 			continue;
 		}
-		start = str;
-		str++;
-		while (print_flags(str, &flags)) /* while char at str is flag char */
+		p++;
+		if (*p == '%')
 		{
-			str++; /* next char */
+			buffer[buffer_len++] = *p;
+			if (buffer_len == BUFF_SIZE)
+			{
+				count += write(STDOUT_FILENO, buffer, buffer_len);
+				buffer_len = 0;
+			}
+			p++;
+			count++;
+			continue;
 		}
-		str = get_width(str, &flags, args);
-		str = get_precision(str, &flags, args);
-		if (get_modify(str, &flags))
-			str++;
-		if (!handle_format_specifier(str))
-			count += print_range(start, str,
-				flags.length || flags.height ? str - 1 : 0);
-		else
-			count += format_function(str, args, &flags);
+		int width = -1, precision = -1;
+		while (*p && is_flag(*p))
+			update_flag(*p++, &width, &precision);
+		if (*p && isdigit(*p))
+			width = extract_number(&p);
+		if (*p == '.')
+		{
+			p++;
+			if (*p && isdigit(*p))
+				precision = extract_number(&p);
+			else
+				precision = 0;
+		}
+		char specifier = *p++;
+		switch (specifier)
+		{
+			case 'c':
+				count += handle_char(buffer, &buffer_len, va_arg(args, int));
+				break;
+			case 's':
+				count += handle_string(buffer, &buffer_len, va_arg(args, char *), precision);
+				break;
+			case 'd':
+			case 'i':
+				count += handle_integer(buffer, &buffer_len, va_arg(args, int), width, precision);
+				break;
+			case 'u':
+				count += handle_unsigned(buffer, &buffer_len, va_arg(args, unsigned int), width, precision);
+				break;
+			case 'o':
+				count += handle_octal(buffer, &buffer_len, va_arg(args, unsigned int), width, precision);
+				break;
+			case 'x':
+				count += handle_hex(buffer, &buffer_len, va_arg(args, unsigned int), width, precision, 0);
+				break;
+			case 'X':
+				count += handle_hex(buffer, &buffer_len, va_arg(args, unsigned int), width, precision, 1);
+				break;
+			case 'p':
+				count += handle_pointer(buffer, &buffer_len, va_arg(args, void *));
+				break;
+			default:
+				buffer[buffer_len++] = '%';
+				if (buffer_len == BUFF_SIZE)
+				{
+					count += write(STDOUT_FILENO, buffer, buffer_len);
+					buffer_len = 0;
+				}
+				if (*p)
+				{
+					buffer[buffer_len++] = *p++;
+					if (buffer_len == BUFF_SIZE)
+					{
+						count += write(STDOUT_FILENO, buffer, buffer_len);
+						buffer_len = 0;
+					}
+				}
+				count++;
+				break;
+		}
 	}
-	_putchar(FLUSH);
-
+	if (buffer_len > 0)
+		count += write(STDOUT_FILENO, buffer, buffer_len);
 	va_end(args);
-	return (count);
+	return count;
 }
