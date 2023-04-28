857d912b33345f91336c125ce1ca60d5a7ea859c
rafmury <126829982+rafmury@users.noreply.github.com>
2023-04-28 23:44:41 +0300

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -10,6 +10,7 @@
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
+ * by @ridhwan497 and @rafmury
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
